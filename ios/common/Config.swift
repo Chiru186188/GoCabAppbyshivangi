e95e1207b4deb5b4c90c00e173134a4335a557c1
ShivendraSengar <133664075+Chiru186188@users.noreply.github.com>
2024-05-14 11:26:52 +0530

    First Commit

---
+++ ios/common/Config.swift
@@ -0,0 +1,35 @@
+//
+//  Config.swift
+//  Shared
+//
+//  Created by Manly Man on 11/22/19.
+//  Copyright © 2019 Innomalist. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+ //   static var Backend: String = "http://demo.ridy.io/"
+    
+   // static var Backend: String = "https://api.go-cabs.com/"
+ //   static var Backend: String = "https://api-stage.go-cabs.com/"
+    
+    static var Backend: String = "https://api-dev.go-cabs.com/"
+
+
+    
+    
+
+    static var Version: String {
+        get {
+            return self.Info["CFBundleVersion"] as! String
+        }
+    }
+    
+    static var Info: [String:Any] {
+        get {
+            let path = Bundle.main.path(forResource: "Info", ofType: "plist")!
+            return NSDictionary(contentsOfFile: path) as! [String: Any]
+        }
+    }
+}
