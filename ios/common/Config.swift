bebd3fc8fedf33d2c6add3e0258742d654a45687
Chirag Wadhwa <133664075+Chiru186188@users.noreply.github.com>
2024-06-19 12:12:20 +0530

    Update Config.swift

--- ios/common/Config.swift
+++ ios/common/Config.swift
@@ -9,7 +9,6 @@
 import Foundation
 
 class Config {
-    //   static var Backend: String = "http://demo.ridy.io/"
     
     // static var Backend: String = "https://api.go-cabs.com/"
     //   static var Backend: String = "https://api-stage.go-cabs.com/"
